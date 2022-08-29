364023505969cec7b2b9d9d8b1c012b256c99e3b
hunnybunnyart <hunnybunstudios@gmail.com>
2022-08-29 15:58:10 -0400

    Business Card Level

--- production/src/App.js
+++ production/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo192.png';
+import logo from './logo.jpg';
 import './App.css';
 
 function App() {
@@ -6,17 +6,34 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p><b>Art, Apparel, and Accessories</b><br/><br/>Coming Soon</p>
+        <span>Support Hunny Bunny by following me on social media.<br/><br/>It would be awesome if you could drop a few likes too, so the algorithm actually shows my art.</span>
+        <div className="App-links">
+          <a
+            className="App-link"
+            href="https://www.instagram.com/bunniebunbunzzz/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Instagram
+          </a>
+          <a
+            className="App-link"
+            href="https://www.facebook.com/HunnyBunnyStudios"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>
+          <a
+            className="App-link"
+            href="https://twitter.com/bunniebunbunzzz"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </div>
       </header>
     </div>
   );
